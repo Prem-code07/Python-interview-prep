69108e074e94501d109df9bd47fab2161c420c1f
Pream code <preamanandasaint99@gmail.com>
2025-06-22 23:36:10 +0530

    It's an modigfied code of find maximum in the list.

--- find_max_list.py
+++ find_max_list.py
@@ -1,7 +1,7 @@
 a=[5,7,3,2,9]
-minimum=a[0]
+maximum=a[0]
 for num in a:
-    if num>minimum:
-        minimum=num
-print(f"The maximum value is {minimum}.")
-# print(max(a))
+    if num>maximum:
+        maximum=num
+print(f"The maximum value is {maximum}.")
+
